3f3b6d00dd89a374795959394374cd80e2bf76dd
juan <juan@xbps>
2009-08-17 19:07:20 +0200

    Initial import of xbps with code as August '09.
    --HG--
    extra : convert_revision : juan%40xbps-20090817170720-amxxac4a2e8bza1j

---
+++ vars.mk
@@ -0,0 +1,16 @@
+# Common variables.
+
+PREFIX	?= /usr/local
+SBINDIR	?= $(PREFIX)/sbin
+LIBDIR	?= $(PREFIX)/lib
+ETCDIR	?= $(PREFIX)/etc
+TOPDIR	?= ..
+INSTALL_STRIPPED ?= -s
+
+LDFLAGS += -L$(TOPDIR)/lib -L$(PREFIX)/lib -lxbps
+CPPFLAGS += -I$(TOPDIR)/include -D_BSD_SOURCE -D_XOPEN_SOURCE=600
+CPPFLAGS += -D_GNU_SOURCE
+WARNFLAGS ?= -pedantic -std=c99 -Wall -Wextra -Werror -Wshadow -Wformat=2
+WARNFLAGS += -Wmissing-declarations -Wcomment -Wunused-macros -Wendif-labels
+WARNFLAGS += -Wcast-qual -Wcast-align -Wconversion
+CFLAGS += $(WARNFLAGS) -O2 -fPIC -DPIC
