becfac0c0805e465aa9b5f42f8595a26aa4edf92
Juan RP <xtraeme@gmail.com>
2009-10-16 16:14:19 +0200

    vars.mk: add DEBUG option.
    --HG--
    extra : convert_revision : xtraeme%40gmail.com-20091016141419-ngopy0o6sv8uuxab

--- vars.mk
+++ vars.mk
@@ -13,6 +13,11 @@ ETCDIR	?= $(PREFIX)/etc
 TOPDIR	?= ..
 INSTALL_STRIPPED ?= -s
 
+ifdef DEBUG
+INSTALL_STRIPPED=
+DEBUG_FLAGS = -g
+endif
+
 STATIC_LIBS ?= -lprop -lpthread -larchive
 LDFLAGS +=  -L$(TOPDIR)/lib -L$(PREFIX)/lib -lxbps
 CPPFLAGS += -I$(TOPDIR)/include -D_BSD_SOURCE -D_XOPEN_SOURCE=600
@@ -20,4 +25,4 @@ CPPFLAGS += -D_GNU_SOURCE
 WARNFLAGS ?= -pedantic -std=c99 -Wall -Wextra -Werror -Wshadow -Wformat=2
 WARNFLAGS += -Wmissing-declarations -Wcomment -Wunused-macros -Wendif-labels
 WARNFLAGS += -Wcast-qual -Wcast-align -Wconversion
-CFLAGS += $(WARNFLAGS) -O2 -fPIC -DPIC
+CFLAGS = $(DEBUG_FLAGS) $(WARNFLAGS) -O2 -fPIC -DPIC
