e9ef83517ee3db8bcb535620264b8474e0c397ff
Juan RP <xtraeme@gmail.com>
2009-10-20 09:45:37 +0200

    vars.mk: build bins with -fstack-protector-all and -Wstack-protector.
    --HG--
    extra : convert_revision : xtraeme%40gmail.com-20091020074537-j84wcoa5mdtt13g2

--- vars.mk
+++ vars.mk
@@ -17,5 +17,5 @@ CPPFLAGS += -I$(TOPDIR)/include -D_BSD_SOURCE -D_XOPEN_SOURCE=600
 CPPFLAGS += -D_GNU_SOURCE
 WARNFLAGS ?= -pedantic -std=c99 -Wall -Wextra -Werror -Wshadow -Wformat=2
 WARNFLAGS += -Wmissing-declarations -Wcomment -Wunused-macros -Wendif-labels
-WARNFLAGS += -Wcast-qual -Wcast-align -Wconversion
-CFLAGS = $(DEBUG_FLAGS) $(WARNFLAGS) -O2 -fPIC -DPIC
+WARNFLAGS += -Wcast-qual -Wcast-align -Wconversion -Wstack-protector
+CFLAGS = $(DEBUG_FLAGS) $(WARNFLAGS) -fstack-protector-all -O2 -fPIC -DPIC
