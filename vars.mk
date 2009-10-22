534f7f0306042dff9a2f59ccb25cd3d03fe8ef61
Juan RP <xtraeme@gmail.com>
2009-10-22 14:48:33 +0200

    vars.mk: _BSD_SOURCE does not need to be defined.
    --HG--
    extra : convert_revision : xtraeme%40gmail.com-20091022124833-g1z0iznkjw4nayjs

--- vars.mk
+++ vars.mk
@@ -13,8 +13,7 @@ endif
 
 STATIC_LIBS ?= -lprop -lpthread -larchive
 LDFLAGS +=  -L$(TOPDIR)/lib -L$(PREFIX)/lib -lxbps
-CPPFLAGS += -I$(TOPDIR)/include -D_BSD_SOURCE -D_XOPEN_SOURCE=600
-CPPFLAGS += -D_GNU_SOURCE
+CPPFLAGS += -I$(TOPDIR)/include -D_XOPEN_SOURCE=600 -D_GNU_SOURCE
 WARNFLAGS ?= -pedantic -std=c99 -Wall -Wextra -Werror -Wshadow -Wformat=2
 WARNFLAGS += -Wmissing-declarations -Wcomment -Wunused-macros -Wendif-labels
 WARNFLAGS += -Wcast-qual -Wcast-align -Wconversion -Wstack-protector
