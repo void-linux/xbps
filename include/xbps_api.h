736fcbfdd06eb04b5b52f56cdd43c30c6a3da043
Juan RP <xtraeme@gmail.com>
2009-11-09 02:45:03 +0100

    xbps_api.h: bump XBPS_RELVER to 20091109.
    --HG--
    extra : convert_revision : xtraeme%40gmail.com-20091109014503-4lwvkwrz2wva88pn

--- include/xbps_api.h
+++ include/xbps_api.h
@@ -37,7 +37,7 @@
 #include <archive_entry.h>
 
 /* Current release version */
-#define XBPS_RELVER		"20091107"
+#define XBPS_RELVER		"20091109"
 
 /* Default root PATH for xbps to store metadata info. */
 #define XBPS_META_PATH		"/var/db/xbps"
