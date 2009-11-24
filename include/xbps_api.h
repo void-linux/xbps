dc68e31ad9bfc394ec0a4b821c7aa68ab90f9373
Juan RP <xtraeme@gmail.com>
2009-11-24 03:07:49 +0000

    Bump XBPS_RELVER to 20091124 for xbps-repo genindex fix.
    --HG--
    extra : convert_revision : xtraeme%40gmail.com-20091124030749-n313t2s3msc3p9nu

--- include/xbps_api.h
+++ include/xbps_api.h
@@ -38,7 +38,7 @@
 #include <archive_entry.h>
 
 /* Current release version */
-#define XBPS_RELVER		"20091123"
+#define XBPS_RELVER		"20091124"
 
 /* Default root PATH for xbps to store metadata info. */
 #define XBPS_META_PATH		"/var/db/xbps"
