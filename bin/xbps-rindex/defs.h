1999fbd77125f1e5725cf82c1450c61e1d524a5c
Juan RP <xtraeme@gmail.com>
2013-08-16 10:26:36 +0200

    Relax libarchive version requirement to 2.8.0.

--- bin/xbps-rindex/defs.h
+++ bin/xbps-rindex/defs.h
@@ -45,4 +45,36 @@ int	remove_obsoletes(struct xbps_handle *, const char *);
 int	repodata_flush(struct xbps_handle *, const char *,
 		xbps_dictionary_t, xbps_dictionary_t);
 
+/* libarchive compat */
+#if ARCHIVE_VERSION_NUMBER >= 3000000
+
+#define archive_read_support_compression_gzip(x) \
+	archive_read_support_filter_gzip(x)
+
+#define archive_read_support_compression_bzip2(x) \
+	archive_read_support_filter_bzip2(x)
+
+#define archive_read_support_compression_xz(x) \
+	archive_read_support_filter_xz(x)
+
+#define archive_write_set_compression_gzip(x) \
+	archive_write_add_filter_gzip(x)
+
+#define archive_write_set_compression_bzip2(x) \
+	archive_write_add_filter_bzip2(x)
+
+#define archive_write_set_compression_xz(x) \
+	archive_write_add_filter_xz(x)
+
+#define archive_read_finish(x) \
+	archive_read_free(x)
+
+#define archive_write_finish(x) \
+	archive_write_free(x)
+
+#define archive_compression_name(x) \
+	archive_filter_name(x, 0)
+
+#endif
+
 #endif /* !_XBPS_RINDEX_DEFS_H_ */
