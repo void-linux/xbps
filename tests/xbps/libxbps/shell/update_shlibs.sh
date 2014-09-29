43b450d6fb8f6c9c4a4f32d57313e773312e0e11
Juan RP <xtraeme@gmail.com>
2014-09-29 14:54:43 +0200

    tests: new test to verify another shlibs revdep case (currently it fails).

--- tests/xbps/libxbps/shell/update_shlibs.sh
+++ tests/xbps/libxbps/shell/update_shlibs.sh
@@ -118,8 +118,46 @@ shlib_bump_incomplete_revdep_in_trans_body() {
 	atf_check_equal $? 8
 }
 
+atf_test_case shlib_bump_revdep_diff
+
+shlib_bump_revdep_diff_head() {
+	atf_set "descr" "Tests for pkg updates: revdep does not require previous shlibs"
+}
+
+shlib_bump_revdep_diff_body() {
+	mkdir -p repo pkg
+	cd repo
+	xbps-create -A noarch -n A-1.0_1 -s "A pkg" --shlib-provides "liba.so.1" ../pkg
+	atf_check_equal $? 0
+	xbps-create -A noarch -n B-1.0_1 -s "B pkg" --shlib-provides "libb.so.1" ../pkg
+	atf_check_equal $? 0
+	xbps-create -A noarch -n C-1.0_1 -s "C pkg" --dependencies "A>=0 B>=0" --shlib-requires "liba.so.1 libb.so.1" ../pkg
+	atf_check_equal $? 0
+	xbps-rindex -a *.xbps
+	atf_check_equal $? 0
+	cd ..
+
+	xbps-install -C empty.conf -r root --repository=$PWD/repo -yvd C
+	atf_check_equal $? 0
+
+	cd repo
+	xbps-create -A noarch -n A-2.0_1 -s "A pkg" --shlib-provides "liba.so.2" ../pkg
+	atf_check_equal $? 0
+	xbps-create -A noarch -n B-2.0_1 -s "B pkg" --shlib-provides "libb.so.2" ../pkg
+	atf_check_equal $? 0
+	xbps-create -A noarch -n C-2.0_1 -s "C pkg" --dependencies "A>=2.0" --shlib-requires "liba.so.2" ../pkg
+	atf_check_equal $? 0
+	xbps-rindex -a *.xbps
+	atf_check_equal $? 0
+	cd ..
+
+	xbps-install -C empty.conf -r root --repository=$PWD/repo -yuvd
+	atf_check_equal $? 0
+}
+
 atf_init_test_cases() {
 	atf_add_test_case shlib_bump
 	atf_add_test_case shlib_bump_incomplete_revdep_in_trans
 	atf_add_test_case shlib_bump_revdep_in_trans
+	atf_add_test_case shlib_bump_revdep_diff
 }
