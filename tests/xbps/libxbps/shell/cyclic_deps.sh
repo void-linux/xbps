e16ca06d5b28461bfb5ebbdc595e2b3efbba48e2
Juan RP <xtraeme@voidlinux.eu>
2015-10-21 09:29:21 +0200

    tests/xbps/libxbps/shell/cyclic_deps.sh: use atf_expect_death instead.

--- tests/xbps/libxbps/shell/cyclic_deps.sh
+++ tests/xbps/libxbps/shell/cyclic_deps.sh
@@ -54,7 +54,36 @@ cyclic_dep_vpkg2_body() {
 	atf_check_equal $? 40
 }
 
+atf_test_case cyclic_dep_full
+
+cyclic_dep_full_head() {
+	atf_set "descr" "Tests for cyclic deps: verify fulldeptree"
+}
+
+cyclic_dep_full_body() {
+	atf_expect_death "Known bug: see https://github.com/voidlinux/xbps/issues/92"
+	mkdir some_repo
+	mkdir -p pkg_{A,B}/usr/bin
+	cd some_repo
+	xbps-create -A noarch -n A-1.0_1 -s "A pkg" --dependencies "B>=0" ../pkg_A
+	atf_check_equal $? 0
+	xbps-create -A noarch -n B-1.0_1 -s "B pkg" --dependencies "A>=0" ../pkg_B
+	atf_check_equal $? 0
+
+	xbps-rindex -d -a $PWD/*.xbps
+	atf_check_equal $? 0
+	cd ..
+
+	xbps-install -r root --repository=$PWD/some_repo -dy B
+	atf_check_equal $? 0
+	xbps-query -r root --fulldeptree -d B
+	atf_check_equal $? 0
+	xbps-remove -r root -Ryvd B
+	atf_check_equal $? 0
+}
+
 atf_init_test_cases() {
 	atf_add_test_case cyclic_dep_vpkg
 	atf_add_test_case cyclic_dep_vpkg2
+	atf_add_test_case cyclic_dep_full
 }
