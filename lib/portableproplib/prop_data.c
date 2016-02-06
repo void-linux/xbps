2130d5daa61e2a3e998d879f6ab3852838385f86
Juan RP <xtraeme@voidlinux.eu>
2016-02-06 09:41:27 +0100

    proplib/prop_data: avoid 0 sized allocation reported by clang-analyzer.

--- lib/portableproplib/prop_data.c
+++ lib/portableproplib/prop_data.c
@@ -565,8 +565,8 @@ _prop_data_internalize(prop_stack_t stack, prop_object_t *obj,
 						NULL) == false)
 		return (true);
 
-	if (len + 1 >= SIZE_MAX)
-		return true;
+	if (len >= SIZE_MAX)
+		len = 1;
 	/*
 	 * Always allocate one extra in case we don't land on an even byte
 	 * boundary during the decode.
