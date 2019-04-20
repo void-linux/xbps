c658e41747d2aee58b0d20cf1d9dec42c31ef9e4
Juan RP <xtraeme@gmail.com>
2019-04-20 19:04:13 +0200

    fulldeptree: make sure there are no dups.
    Added a test case to verify that dup deps are discarded
    completely.

--- tests/xbps/libxbps/shell/incorrect_deps_test.sh
+++ tests/xbps/libxbps/shell/incorrect_deps_test.sh
@@ -69,8 +69,37 @@ incorrect_dep_issue45_body() {
 	atf_check_equal $? 0
 }
 
+atf_test_case incorrect_dep_dups
+
+incorrect_dep_dups_head() {
+	atf_set "descr" "Test for package deps: duplicated deps in fulldeptree"
+}
+
+incorrect_dep_dups_body() {
+	mkdir some_repo
+	mkdir -p pkg_A/usr/bin pkg_B/usr/bin
+	cd some_repo
+	xbps-create -A noarch -n A-1.0_1 -s "A pkg" ../pkg_A
+	atf_check_equal $? 0
+	xbps-create -A noarch -n B-1.0_1 -s "B pkg" --dependencies "A>=0 A>=0" ../pkg_B
+	atf_check_equal $? 0
+
+	xbps-rindex -d -a $PWD/*.xbps
+	atf_check_equal $? 0
+	cd ..
+
+	xbps-install -C empty.conf -r root --repository=$PWD/some_repo -dy B
+	atf_check_equal $? 0
+
+	xbps-query -C empty.conf -r root --fulldeptree -x B
+	out=$(xbps-query -C empty.conf -r root --fulldeptree -x B)
+	set -- $out
+	atf_check_equal "$1 $2" "B-1.0_1 A-1.0_1"
+}
+
 atf_init_test_cases() {
 	atf_add_test_case incorrect_dep
 	atf_add_test_case incorrect_dep_vpkg
 	atf_add_test_case incorrect_dep_issue45
+	atf_add_test_case incorrect_dep_dups
 }
