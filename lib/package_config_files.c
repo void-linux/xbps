1d8b95a993acb45e54839cd9d19fadba8fb5b9fe
Juan RP <xtraeme@gmail.com>
2019-04-21 13:31:10 +0200

    package_config_files.c: fix a shadowed variable declaration.
    Found by pcc.

--- lib/package_config_files.c
+++ lib/package_config_files.c
@@ -64,7 +64,7 @@ xbps_entry_install_conf_file(struct xbps_handle *xhp,
 			     struct archive_entry *entry,
 			     const char *entry_pname,
 			     const char *pkgver,
-			     bool symlink)
+			     bool mysymlink)
 {
 	xbps_object_t obj, obj2;
 	xbps_object_iterator_t iter, iter2;
@@ -88,7 +88,7 @@ xbps_entry_install_conf_file(struct xbps_handle *xhp,
 	xbps_dbg_printf(xhp, "%s: processing conf_file %s\n",
 	    pkgver, entry_pname);
 
-	if (pkg_filesd == NULL || symlink) {
+	if (pkg_filesd == NULL || mysymlink) {
 		/*
 		 * 1. File exists on disk but it's not managed by the same package.
 		 * 2. File exists on disk as symlink.
