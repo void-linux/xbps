f6b2fae13b8f9b9e055564327727d26f21a59f29
Piotr Wójcik <chocimier@tlen.pl>
2021-04-09 23:09:13 +0200

    dewey.c: convert to UTF-8

--- lib/external/dewey.c
+++ lib/external/dewey.c
@@ -1,7 +1,7 @@
 /* $NetBSD: dewey.c,v 1.1.1.3 2009/03/08 14:51:37 joerg Exp $ */
 
 /*
- * Copyright © 2002 Alistair G. Crooks.  All rights reserved.
+ * Copyright Â© 2002 Alistair G. Crooks.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
