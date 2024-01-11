06981f1d8c3cc67c9e7d4b2491375cf1c29ce7cf
Duncan Overbruck <mail@duncano.de>
2024-01-11 17:13:11 +0100

    lib: remove xbps_entry_install_conf_file

--- include/xbps_api_impl.h
+++ include/xbps_api_impl.h
@@ -84,9 +84,6 @@ bool HIDDEN xbps_remove_pkg_from_array_by_pkgver(xbps_array_t, const char *);
 void HIDDEN xbps_fetch_set_cache_connection(int, int);
 void HIDDEN xbps_fetch_unset_cache_connection(void);
 int HIDDEN xbps_cb_message(struct xbps_handle *, xbps_dictionary_t, const char *);
-int HIDDEN xbps_entry_install_conf_file(struct xbps_handle *, xbps_dictionary_t,
-		xbps_dictionary_t, struct archive_entry *, const char *,
-		const char *, bool);
 xbps_dictionary_t HIDDEN xbps_find_virtualpkg_in_conf(struct xbps_handle *,
 		xbps_dictionary_t, const char *);
 xbps_dictionary_t HIDDEN xbps_find_pkg_in_dict(xbps_dictionary_t, const char *);
