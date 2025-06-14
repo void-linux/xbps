7d4d65ff01a61919016655c27a7f7a43981d93f5
Duncan Overbruck <mail@duncano.de>
2025-06-14 02:44:40 +0200

    tests: fix more tests using wrong architectures from /usr/share/xbps.d or env

--- tests/xbps/libxbps/shell/scripts_test.sh
+++ tests/xbps/libxbps/shell/scripts_test.sh
@@ -29,25 +29,20 @@ script_nargs_body() {
 	echo "A-1.0_1" > pkg_A/usr/bin/foo
 	create_script pkg_A/INSTALL
 
-	cd some_repo
-	xbps-create -A noarch -n A-1.0_1 -s "A pkg" ../pkg_A
-	atf_check_equal $? 0
-	xbps-rindex -d -a $PWD/*.xbps
-	atf_check_equal $? 0
-	cd ..
-	xbps-install -C empty.conf -r root --repository=$PWD/some_repo -y A
-	atf_check_equal $? 0
+	unset XBPS_ARCH XBPS_TARGET_ARCH
 
-	rval=0
-	xbps-reconfigure -C empty.conf -r root -f A 2>out
-	out="$(cat out)"
-	expected="post A 1.0_1 no no $(uname -m)"
-	if [ "$out" != "$expected" ]; then
-		echo "out: '$out'"
-		echo "expected: '$expected'"
-		rval=1
-	fi
-	atf_check_equal $rval 0
+	arch=$(xbps-uhelper -r root arch)
+	cd some_repo
+	atf_check -o ignore -- xbps-create -A noarch -n A-1.0_1 -s "A pkg" ../pkg_A
+	# XXX: xbps-rindex has no root flag to ignore /usr/share/xbps.d/xbps-arch.conf
+	XBPS_ARCH="$arch" atf_check -o ignore -- xbps-rindex -a $PWD/*.xbps
+	cd ..
+	atf_check -o ignore \
+		-e inline:"pre A 1.0_1 no no ${arch}\npost A 1.0_1 no no ${arch}\n" -- \
+		xbps-install -C empty.conf -r root --repository=$PWD/some_repo -y A
+
+	atf_check -o ignore -e inline:"post A 1.0_1 no no ${arch}\n" -- \
+		xbps-reconfigure -C empty.conf -r root -f A
 }
 
 atf_test_case script_arch
