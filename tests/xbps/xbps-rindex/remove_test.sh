7e3dcc0a8b6c82aa4236a33fc75f315488d047c9
Duncan Overbruck <mail@duncano.de>
2025-06-15 20:33:12 +0200

    tests: check if xbps-rindex --remove-obsoletes actually removes files

--- tests/xbps/xbps-rindex/remove_test.sh
+++ tests/xbps/xbps-rindex/remove_test.sh
@@ -11,26 +11,23 @@ noremove_stage_head() {
 noremove_stage_body() {
 	mkdir -p some_repo pkg_A pkg_B
 	cd some_repo
-	xbps-create -A noarch -n foo-1.0_1 -s "foo pkg" --shlib-provides "foo.so.1" ../pkg_A
-	atf_check_equal $? 0
-	xbps-create -A noarch -n bar-1.0_1 -s "foo pkg" --shlib-requires "foo.so.1" ../pkg_B
-	atf_check_equal $? 0
-	xbps-rindex -d -a $PWD/*.xbps
-	atf_check_equal $? 0
-	xbps-create -A noarch -n foo-1.1_1 -s "foo pkg" --shlib-provides "foo.so.2" ../pkg_A
-	atf_check_equal $? 0
-	xbps-rindex -d -a $PWD/*.xbps
-	atf_check_equal $? 0
-	atf_check -o inline:"    2 $PWD (Staged) (RSA unsigned)\n" -- \
-		xbps-query -r ../root -i --repository=$PWD -L
-	xbps-rindex -r some_repo
-	atf_check_equal $? 0
-	[ -f foo-1.0_1* ]
-	atf_check_equal $? 0
-	[ -f foo-1.1_1* ]
-	atf_check_equal $? 0
-	[ -f bar-1.0_1* ]
-	atf_check_equal $? 0
+	atf_check -o ignore -- xbps-create -A noarch -n foo-1.0_1 -s "foo pkg" --shlib-provides "foo.so.1" ../pkg_A
+	atf_check -o ignore -- xbps-create -A noarch -n bar-1.0_1 -s "foo pkg" --shlib-requires "foo.so.1" ../pkg_B
+	atf_check -o ignore -- xbps-create -A noarch -n deleteme-1.0_1 -s "deleteme pkg" ../pkg_B
+	atf_check -o ignore -e ignore -- xbps-rindex -a $PWD/*.xbps
+	atf_check -o ignore -- xbps-create -A noarch -n deleteme-1.1_1 -s "deleteme pkg" ../pkg_B
+	atf_check -o ignore -e ignore -- xbps-rindex -a $PWD/*.xbps
+	atf_check -o ignore -- xbps-create -A noarch -n foo-1.1_1 -s "foo pkg" --shlib-provides "foo.so.2" ../pkg_A
+	atf_check -o ignore -e ignore -- xbps-rindex -a $PWD/*.xbps
+	cd ..
+	atf_check -o inline:"    3 $PWD/some_repo (Staged) (RSA unsigned)\n" -- \
+		xbps-query -r ../root -i --repository=some_repo -L
+	atf_check -o inline:"Removed obsolete package \`deleteme-1.0_1.noarch.xbps'.\n" -- xbps-rindex -r some_repo
+	[ -f some_repo/foo-1.0_1* ] || atf_fail "foo-1.0_1 doesn't exist"
+	[ -f some_repo/foo-1.1_1* ] || atf_fail "foo-1.1_1 doesn't exist"
+	[ -f some_repo/bar-1.0_1* ] || atf_fail "bar-1.0_1 doesn't exist"
+	[ -f some_repo/deleteme-1.0_1* ] && atf_fail "deleteme-1.0_1 still exists"
+	[ -f some_repo/deleteme-1.1_1* ] || atf_fail "deleteme-1.1_1 doesn't exist"
 }
 
 atf_init_test_cases() {
