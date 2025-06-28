d459f5b58627c03300e9a5b449353e662ea5bfe7
Duncan Overbruck <mail@duncano.de>
2025-06-28 19:06:22 +0200

    lib: handle errors and avoid allocs in transaction conflict check

--- include/xbps_api_impl.h
+++ include/xbps_api_impl.h
@@ -89,7 +89,7 @@ xbps_dictionary_t HIDDEN xbps_find_virtualpkg_in_array(struct xbps_handle *,
 bool HIDDEN xbps_transaction_check_revdeps(struct xbps_handle *, xbps_array_t);
 bool HIDDEN xbps_transaction_check_shlibs(struct xbps_handle *, xbps_array_t);
 bool HIDDEN xbps_transaction_check_replaces(struct xbps_handle *, xbps_array_t);
-bool HIDDEN xbps_transaction_check_conflicts(struct xbps_handle *, xbps_array_t);
+int HIDDEN xbps_transaction_check_conflicts(struct xbps_handle *, xbps_array_t);
 bool HIDDEN xbps_transaction_store(struct xbps_handle *, xbps_array_t, xbps_dictionary_t, bool);
 int HIDDEN xbps_transaction_init(struct xbps_handle *);
 int HIDDEN xbps_transaction_files(struct xbps_handle *,
