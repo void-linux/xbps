3800c894fdb776e3c21993a3814067c9c6c0532a
Duncan Overbruck <mail@duncano.de>
2025-07-30 18:37:11 +0200

    lib: use PRINTF_LIKE instead of pragma's to ignore -Wformat-nonliteral

--- lib/cb_util.c
+++ lib/cb_util.c
@@ -28,13 +28,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
-#include <errno.h>
 
 #include "xbps_api_impl.h"
-
-#ifdef __clang__
-#pragma clang diagnostic ignored "-Wformat-nonliteral"
-#endif
 
 void HIDDEN
 xbps_set_cb_fetch(struct xbps_handle *xhp,
@@ -62,7 +57,7 @@ xbps_set_cb_fetch(struct xbps_handle *xhp,
 	(*xhp->fetch_cb)(&xfcd, xhp->fetch_cb_data);
 }
 
-int HIDDEN
+int HIDDEN PRINTF_LIKE(5, 6)
 xbps_set_cb_state(struct xbps_handle *xhp,
 		  xbps_state_t state,
 		  int err,
