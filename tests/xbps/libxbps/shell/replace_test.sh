5e07088c44f993719adbd0f297cf9e12e2f20fc1
classabbyamp <void@placeviolette.net>
2025-12-31 19:40:07 -0500

    tests: add test case for replaces
    Co-authored-by: Piotr Wójcik <chocimier@tlen.pl>

--- tests/xbps/libxbps/shell/replace_test.sh
+++ tests/xbps/libxbps/shell/replace_test.sh
@@ -613,6 +613,32 @@ replace_automatically_installed_dep3_body() {
 	atf_check_equal $(xbps-query -C xbps.d -r root -p automatic-install B) "yes"
 }
 
+atf_test_case dont_replace_alternative_pkg_during_install
+
+dont_replace_alternative_pkg_during_install_head() {
+	atf_set "descr" "Tests for package replace: install a package that has a pair replacing each other"
+}
+
+dont_replace_alternative_pkg_during_install_body() {
+	mkdir some_repo root
+	mkdir -p pkg_A/usr/bin empty
+	echo "A-1.0_1" > pkg_A/usr/bin/foo
+	cd some_repo
+	xbps-create -A noarch -n A-1.0_1 -s "A pkg" --replaces "B>=0" ../pkg_A
+	atf_check_equal $? 0
+	xbps-create -A noarch -n B-1.0_1 -s "B pkg" --replaces "A>=0" ../pkg_A
+	atf_check_equal $? 0
+	xbps-rindex -d -a $PWD/*.xbps
+	atf_check_equal $? 0
+	cd ..
+	xbps-install -C xbps.d -r root --repository=$PWD/some_repo -yd B
+	atf_check_equal $? 0
+	result=$(xbps-query -r root -l | wc -l)
+	atf_check_equal $result 1
+	atf_check_equal $(xbps-query -C xbps.d -r root -p state A) ""
+	atf_check_equal $(xbps-query -C xbps.d -r root -p state B) installed
+}
+
 atf_init_test_cases() {
 	atf_add_test_case replace_dups
 	atf_add_test_case replace_ntimes
@@ -630,4 +656,5 @@ atf_init_test_cases() {
 	atf_add_test_case replace_automatically_installed_dep
 	atf_add_test_case replace_automatically_installed_dep2
 	atf_add_test_case replace_automatically_installed_dep3
+	atf_add_test_case dont_replace_alternative_pkg_during_install
 }
