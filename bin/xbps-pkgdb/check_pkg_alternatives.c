839cc370193d298c344d2709b0369fa05a5c858c
vexalous <vexalous@gmail.com>
2026-04-26 14:25:02 -0400

    bin/xbps-pkgdb: fix off-by-one buffer allocation

--- bin/xbps-pkgdb/check_pkg_alternatives.c
+++ bin/xbps-pkgdb/check_pkg_alternatives.c
@@ -85,7 +85,7 @@ relpath(char *from, char *to)
 
 	for (up = -1, from--; from && *from; from = strchr(from + 1, '/'), up++);
 
-	rel = calloc(3 * up + strlen(p), sizeof(char));
+	rel = calloc(3 * up + strlen(p) + 1, sizeof(char));
 
 	while (up--)
 		strcat(rel, "../");
